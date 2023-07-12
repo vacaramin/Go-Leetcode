388232b84e175627a3b9f8a0ebd3c935539d179e
Waqar Amin <vacaramin86@gmail.com>
2023-07-12 06:17:23 +0500

    Reverse Integer added

--- Reverse Integer/main.go
+++ Reverse Integer/main.go
@@ -1,1 +1,7 @@
-package main
+package Reverse_Integer
+
+import "fmt"
+
+func main() {
+	fmt.Println("Reverse Integer")
+}
--- Two Sum/main.go
+++ Two Sum/main.go
@@ -2,12 +2,11 @@ package main
 
 // Two Sum problem
 import (
-	"crypto/rand"
 	"fmt"
 )
 
 func main() {
-	var two_sum int[]
-	
+	var two_sum int
+
 	fmt.Println(two_sum)
 }
