fdc454788b288969884b638b956e6de053807730
Waqar Amin <vacaramin86@gmail.com>
2023-07-12 06:15:12 +0500

    Two Sum and Reverse Integer added

---
+++ Two Sum/main.go
@@ -0,0 +1,13 @@
+package main
+
+// Two Sum problem
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+func main() {
+	var two_sum int[]
+	
+	fmt.Println(two_sum)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vacaramin/Go-Leetcode
+
+go 1.20
